d90dfaf405092263425fd45e39e4011a3c7dbc87
Linkon Sid <linkon@sheba.xyz>
2023-09-11 21:57:09 +0600

    url made optional. dedault image return for nil url

--- Sources/NetworkImage/NetworkImage.swift
+++ Sources/NetworkImage/NetworkImage.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 public struct NetworkImage<Content: View>: View {
-    private let url: URL
+    private let url: URL?
     private let scale: CGFloat
     private let transaction: Transaction
     private let content: (AsyncImagePhase) -> Content
 
-    public init(url: URL, scale: CGFloat = 1.0, transaction: Transaction = Transaction(), @ViewBuilder content: @escaping (AsyncImagePhase) -> Content) {
+    public init(url: URL?, scale: CGFloat = 1.0, transaction: Transaction = Transaction(), @ViewBuilder content: @escaping (AsyncImagePhase) -> Content) {
         self.url = url
         self.scale = scale
         self.transaction = transaction
@@ -14,18 +14,22 @@ public struct NetworkImage<Content: View>: View {
     }
     
     public var body: some View {
-        if let cachedImage = ImageCache[url] {
-            content(.success(cachedImage))
-        } else {
-            AsyncImage(url: url, scale: scale, transaction: transaction) { phase in
-                cachAndRenderView(by: phase)
+        if let url = url {
+            if let cachedImage = ImageCache[url] {
+                content(.success(cachedImage))
+            } else {
+                AsyncImage(url: url, scale: scale, transaction: transaction) { phase in
+                    cachAndRenderView(by: phase, url: url)
+                }
             }
+        } else {
+            Image(systemName: "questionmark")
         }
     }
 }
 
 extension NetworkImage {
-    private func cachAndRenderView(by phase: AsyncImagePhase) -> some View {
+    private func cachAndRenderView(by phase: AsyncImagePhase, url: URL) -> some View {
         if case .success(let image) = phase {
             ImageCache[url] = image
         }
